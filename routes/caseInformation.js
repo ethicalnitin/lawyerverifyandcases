f65a5f465d9f43c7055eff621c75ab9fb1f531bf
Nitin <157195670+ethicalnitin@users.noreply.github.com>
2025-03-26 12:43:09 +0530

    Update caseInformation.js

--- routes/caseInformation.js
+++ routes/caseInformation.js
@@ -10,13 +10,14 @@ function getSessionCookie(req) {
   return req.sessionID || null; // Session ID for debugging
 }
 
- if (req.body.captchaCookies) {
-      req.session.captchaCookies = req.body.captchaCookies;
-    }
+
 
 router.post('/fetchCaseDetails', async (req, res) => {
   try {
     const { court_code, state_code, court_complex_code, case_no, cino } = req.body;
+     if (req.body.captchaCookies) {
+      req.session.captchaCookies = req.body.captchaCookies;
+    }
 
     
     if (!court_code || !state_code || !court_complex_code || !case_no || !cino || !req.session.captchaCookies) {
